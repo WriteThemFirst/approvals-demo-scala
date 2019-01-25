ed7bbbd349f4c4945888c15960d1f2649a0f3249
Michel Daviot <michel@colisweb.com>
2019-01-25 10:28:27 +0100

    :sparkles: demonstrate approving a Path

---
+++ src/test/scala/com/demo/file/FileContentTest.scala
@@ -0,0 +1,14 @@
+package com.demo.file
+
+import java.nio.file.Files._
+
+import com.demo.Approbation
+
+class FileContentTest extends Approbation {
+  "file content" should "be the same" in { approver =>
+  val tempFolder = createTempDirectory("test")
+    val tempFile = createFile(tempFolder.resolve("file.txt"))
+    write(tempFile, "file contents".getBytes)
+    approver.verify(tempFile)
+  }
+}
