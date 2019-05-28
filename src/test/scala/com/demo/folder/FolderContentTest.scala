1b7544360fbbdf2dd7914d0af54a584023b55e99
Michel Daviot <michel@daviot.info>
2019-05-28 10:57:15 +0200

    :white_check_mark: Test for folder approbation

---
+++ src/test/scala/com/demo/folder/FolderContentTest.scala
@@ -0,0 +1,11 @@
+package com.demo.folder
+
+import java.nio.file.Paths
+
+import com.demo.Approbation
+
+class FolderContentTest extends Approbation {
+  "folder content" should "be the same" in { approver =>
+    approver.verify(Paths.get("src/test/resources/com/demo/folder.Feature__demo.json.actual"))
+  }
+}
